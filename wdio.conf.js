3a43a05deb7b7cf3f177bf9f64e6af265e2f0d9f
nickdeichman <nickdeichman@gmail.com>
2024-10-15 12:35:56 +0300

    add headless mode for the firefox

--- wdio.conf.js
+++ wdio.conf.js
@@ -14,13 +14,13 @@ exports.config = {
         browserName: 'firefox',
         acceptInsecureCerts: true,
         'moz:firefoxOptions': {
-            //args: ['-headless']
+            args: ['-headless']
         }
     }
 ],
     logLevel: 'error',
     bail: 0,
-    baseUrl: 'https://cnt-ad31c2aa-2e62-4f9f-a126-3176a613515c.containerhub.tripleten-services.com/',
+    baseUrl: 'https://cnt-8ea77eb2-5264-4e5d-b31b-ac545c9ecfbb.containerhub.tripleten-services.com/',
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
